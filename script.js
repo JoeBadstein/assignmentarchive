79ca0abfaadb006f4d11ffa5ebb2d2c615d95fcd
unknown <erolmunci@gmail.com>
2023-08-26 18:15:02 -0400

    css upheaval

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-async function checkPassword() {
-    const input = document.getElementById('password').value;
-    const response = await fetch(`/.netlify/functions/checkPassword?password=${input}`);
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.getElementById('password');
+    const errorMessage = document.getElementById('error-message');
     
-    if (response.status === 200) {
-      window.location.href = 'success.html';
-    } else {
-      alert('Incorrect password');
+    passwordInput.addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+        checkPassword();
+      }
+    });
+  
+    async function checkPassword() {
+      const input = passwordInput.value;
+      const response = await fetch(`/.netlify/functions/checkPassword?password=${input}`);
+      
+      if (response.status === 200) {
+        sessionStorage.setItem("password", input);
+        window.location.href = 'success.html';
+      } else {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'Incorrect password';
+      }
     }
-  }
+  });
   

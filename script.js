91e0d00fd6d5d7af985dec8ebf4b212a67228dac
unknown <erolmunci@gmail.com>
2023-08-26 18:50:45 -0400

    security

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
           errorMessage.style.display = 'block';
           errorMessage.textContent = 'Incorrect password'+ response.status;
-          alert(response.body);  // Show alert with correct password
+          alert(data.status);  // Show alert with correct password
         }
     }
   });

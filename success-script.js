f48215bb760a26795c779a13fcdaa0e23137869d
unknown <erolmunci@gmail.com>
2023-08-26 18:16:05 -0400

    css upheaval

--- success-script.js
+++ success-script.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const password = sessionStorage.getItem("password");
     
     fetch(`/.netlify/functions/checkPassword?password=${password}`)
-      .then(response => response.status === 200 ? document.getElementById("content").style.display = "block" : window.location.href = "index.html")
+      .then(response => {
+        if (response.status === 200) {
+          document.getElementById("content").style.display = "block";
+        } else {
+          window.location.href = "index.html";
+        }
+      })
       .catch(() => window.location.href = "index.html");
   });
   
